fae6c7e3b07ced2172c692e550e169b8ba67e3ef
Hugo Fournier <hugo.fournier55000@gmail.com>
2022-12-05 22:32:52 +0100

    unsuccessful attept of day5 part one

---
+++ day5/day5.js
@@ -0,0 +1,92 @@
+/***************************************************************************************
+ *                                                                                           
+ * 
+ *            PART ONE
+ * 
+ * 
+ **************************************************************************************/
+// Step 1 -> retrieve dataset
+const sourceFile = "input.txt";
+const fs = require('fs');
+const dataset = fs.readFileSync(sourceFile, 'utf8');
+const parsedDataset = dataset.split(/\r?\n/);
+
+//Step 2 --> parse data 
+
+//first, i will create a map : 
+//       key : the stack
+//       value : array representing the crates for each crate
+
+const stackOfCrates = new Map();
+
+// find where the instructions begin
+let indexEndingStacksOfCrates = 0
+while (parsedDataset[indexEndingStacksOfCrates] !== "") {
+  indexEndingStacksOfCrates++
+}
+console.log(`Index is ${indexEndingStacksOfCrates}`)
+const lineContainingNumberOfStacks = parsedDataset[indexEndingStacksOfCrates-1].trim();
+const numberOfStacks = parseInt(lineContainingNumberOfStacks[lineContainingNumberOfStacks.length-1], 10);
+
+
+for(let lineNumber = 0; lineNumber < numberOfStacks-1; lineNumber++) {
+  const firstCrat = parsedDataset[lineNumber].substring(0, 3)
+  const secondCrat = parsedDataset[lineNumber].substring(4, 7);
+  const thirdCrat = parsedDataset[lineNumber].substring(8, 11);
+
+  const fourthCrat = parsedDataset[lineNumber].substring(12, 15)
+  const fifthCrat = parsedDataset[lineNumber].substring(16, 19);
+  const sixthCrat = parsedDataset[lineNumber].substring(20, 23);
+  
+  const seventhCrat = parsedDataset[lineNumber].substring(24, 27)
+  const eighthCrat = parsedDataset[lineNumber].substring(28, 31);
+  const ninethCrat = parsedDataset[lineNumber].substring(32, 35);
+
+  const allCrat = [];
+  allCrat[0] = firstCrat;
+  allCrat[1] = secondCrat;
+  allCrat[2] = thirdCrat;
+  allCrat[3] = fourthCrat;
+  allCrat[4] = fifthCrat;
+  allCrat[5] = sixthCrat;
+  allCrat[6] = seventhCrat;
+  allCrat[7] = eighthCrat;
+  allCrat[8] = ninethCrat;
+  for (let keyMap = 1; keyMap <= numberOfStacks; keyMap++ ) {
+    if(stackOfCrates.get(keyMap) === undefined && allCrat[keyMap-1] !== "   ") {
+        const crates = [allCrat[keyMap-1]]
+        stackOfCrates.set(keyMap, crates)
+    } else if(allCrat[keyMap-1] !== "   "){
+        const currentCrates = stackOfCrates.get(keyMap)
+        currentCrates[currentCrates.length] = allCrat[keyMap-1]
+        stackOfCrates.set(keyMap, currentCrates)
+    }
+  }
+}
+console.log(stackOfCrates)
+for (let remainingLine = indexEndingStacksOfCrates+1; remainingLine < parsedDataset.length; remainingLine++) {
+    const instructions = parsedDataset[remainingLine].split(" ")
+
+    const indexMove = instructions.findIndex((instruction) => instruction === "move");
+    const numberOfCratesToMove = parseInt(instructions[indexMove+1], 10)
+
+    const indexFrom = instructions.findIndex((instruction) => instruction === "from");
+    const initialStack = parseInt(instructions[indexFrom+1], 10)
+
+    const indexTo = instructions.findIndex((instruction) => instruction === "to");
+    const destinationStack = parseInt(instructions[indexTo+1], 10);
+    console.log(`Numbertomove : ${numberOfCratesToMove}, INITIAL STACK : ${initialStack}, DEST STACK: ${destinationStack}`)
+    // retrieve from initialStack
+    const cratesToTake = stackOfCrates.get(initialStack).slice(0, numberOfCratesToMove)
+    //remove it
+    stackOfCrates.set(initialStack, stackOfCrates.get(initialStack).splice(0, numberOfCratesToMove))
+    //reverse so last element be on top
+    const reversedArray = cratesToTake.reverse()
+    console.log(reversedArray)
+    //update new stack
+    stackOfCrates.set(destinationStack, reversedArray.concat(stackOfCrates.get(destinationStack)))
+    console.log(remainingLine)
+    //console.log(stackOfCrates.get(destinationStack))
+}
+//display word to guess
+//
